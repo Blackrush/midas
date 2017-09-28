816f58c037337156b5cf9a6ea6e89d05e426956b
Blackrush <blackrushx@gmail.com>
2017-09-28 19:46:46 +0200

    Remove Blueprint Table

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import * as Table from '@blueprintjs/table';
 import * as Layout from './components/layout';
 import Money from './components/Money';
 
@@ -11,62 +10,6 @@ const Header = ({name = 'Midas'}) => (
   </h1>
 );
 
-interface Table2State {
-  initialized: boolean;
-  width: number;
-  height: number;
-}
-
-class Table2 extends React.Component<Table.ITableProps> {
-  state: Table2State = {
-    initialized: false,
-    height: 0,
-    width: 0,
-  };
-
-  private table: Table.Table | null;
-
-  render() {
-    return (
-      <div style={this.containerStyle}>
-        <Table.Table
-          {...this.props}
-          ref={table => this.table = table}
-        />
-      </div>
-    );
-  }
-
-  get containerStyle(): React.CSSProperties {
-    if (this.state.initialized) {
-      return {
-        paddingTop: 1,
-        height: 31 + this.state.height,
-        width: 30 + this.state.width,
-      };
-    }
-
-    return {
-      width: '100%',
-    };
-  }
-
-  componentDidMount() {
-    const table = this.table;
-    if (!table) {
-      return;
-    }
-
-    if (table.state.rowHeights && table.state.columnWidths) {
-      this.setState({
-        initialized: true,
-        width: table.grid.getWidth(),
-        height: table.grid.getHeight(),
-      });
-    }
-  }
-}
-
 export default class App extends React.Component {
   render() {
     return (
@@ -77,21 +20,15 @@ export default class App extends React.Component {
 
         <Layout.Row>
           <Layout.Column>
-            <Table2 numRows={20} defaultRowHeight={25} defaultColumnWidth={110}>
-              <Table.Column/>
-              <Table.Column/>
-              <Table.Column/>
-              <Table.Column/>
-              <Table.Column/>
-            </Table2>
+            hello
           </Layout.Column>
 
           <Layout.Column>
-          hello
+            hello
           </Layout.Column>
 
           <Layout.Column>
-          hello
+            hello
           </Layout.Column>
         </Layout.Row>
       </div>
